4f1107b027342117e150e61f2e3be3822cb9d0f3
Vardan <vardan.vanyan@gmail.com>
2022-10-17 14:55:44 -0700

    working electron app w/typscript

--- Electron/main.ts
+++ Electron/main.ts
@@ -20,5 +20,10 @@ app.whenReady().then(() => {
 
 //quits the app when all windows are closed
 app.on('window-all-closed', () => {
-  app.quit();
+  // On macOS it is common for applications and their
+  // menu bar to stay active until the user quits
+  // explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
