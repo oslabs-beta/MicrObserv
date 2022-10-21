db1eb346a6c48c7307d64f1f555afa2f715973d6
Vardan <vardan.vanyan@gmail.com>
2022-10-20 20:03:23 -0700

    frontend-starting-point created

--- Electron/src/App.jsx
+++ Electron/src/App.jsx
@@ -1,9 +1,27 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
-const React = require('react');
+const { useState } = require('react');
 const ReactDOM = require('react-dom');
 
 const App = () => {
+  const [page, updatePage] = useState('login');
+
+  const renderPage = function () {
+    switch (page) {
+      case 'login':
+        //go to login page
+        break;
+      case 'home':
+        //go to home
+        break;
+      case 'settings':
+        //go to settings
+        break;
+      case 'dashboard':
+        //go to dashboard
+        break;
+    }
+  };
   return (
     <div>
       <h1>Hello!!</h1>
