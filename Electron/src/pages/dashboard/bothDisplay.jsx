e03ed94ab94a897953221f2c68df89dcc6943cd4
Vardan <vardan.vanyan@gmail.com>
2022-10-22 10:15:06 -0700

    login and signup html done

---
+++ Electron/src/pages/dashboard/bothDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function BothDisplay() {
+  return (
+   <div>
+    <div>
+      Search Bar
+    </div>
+    <div>
+      Graphs
+    </div>
+   </div>
+  );
+}
