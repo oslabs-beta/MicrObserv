09cb925dfc431b7c4638ec2c7e3be528471cdb4a
Otis-Jones <82004395+Otis-Jones@users.noreply.github.com>
2022-10-25 15:00:20 -0700

    Graph demo

--- Electron/src/pages/dashboard/dashboard.tsx
+++ Electron/src/pages/dashboard/dashboard.tsx
@@ -4,11 +4,11 @@ import TracersDisplay from './tracers/tracersDisplay';
 export default function Dashboard() {
   return (
     <div className='flex w-screen h-[87.5vh]'>
-      <div className='grid ml-4 flex-grow bg-base-300 rounded-box place-items-start'>
+      <div className='grid flex-grow w-1/2 ml-4 bg-base-300 rounded-box place-items-start'>
         <LogsDisplay />
       </div>
       <div className='divider divider-horizontal'></div>
-      <div className='grid mr-4 flex-grow bg-base-300 rounded-box place-items-start'>
+      <div className='grid flex-grow w-1/2 mr-4 bg-base-300 rounded-box place-items-start'>
         <TracersDisplay />
       </div>
     </div>
--- Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
+++ Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import DashboardContainer from '../dashboardContainer';
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+import {CategoryScale} from 'chart.js';
+Chart.register(CategoryScale);
 
 export default function TracersDisplay() {
   return (
-    <DashboardContainer title='Latency'/>
+    <div className='w-full'>
+      <DashboardContainer title='Latency'/>
+      <BarChart />
+    </div>
+
   );
 }
+
+const BarChart = () => {
+  const labels = ["Service A", "Service B", "Service A", "Service B", "Service C", "Service B"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 255, 255)",
+        data: [5, 10, 5, 2, 20, 10],
+      },
+    ],
+  };
+  const options = {
+    indexAxis: 'y' as const,
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart',
+      },
+    },
+  };
+  return (
+    <div className='flex'>
+      <Bar options={options} data={data} className='object-fit shrink'/>
+    </div>
+  );
+};
