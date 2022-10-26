5ee0004b2b29c1ff624f4685a7d98c5a6baf0f4e
Chance <kupersmith3.6@gmail.com>
2022-10-26 11:53:36 -0500

    setup ipc communication for desktop app
    Bug: pg queries are being skipped without throwing errors? see line 13-15 in main.js for example

--- Electron/main.js
+++ Electron/main.js
@@ -1,17 +1,51 @@
-console.log('Electron is Running');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const { Pool } = require('pg');
+console.log('MicrObserv Desktop is running');
+
+const myURI = 'postgres://nzknncbd:AzIp1howQ8DKmTlflRP18UNTisXgzBsa@otto.db.elephantsql.com/nzknncbd';
+const URI = process.env.PG_URI || myURI;
+const pool = new Pool({
+  connectionString: URI
+});
+
+const pgQuery = async (text, params, callback)=> pool.query(text, params, callback);
+console.log('before');
+pool.query('SELECT * FROM Logs;').then((data)=>console.log(data));
+console.log('after');
+let win;
+
+const getData = async (serviceName) => {
+  try{
+    console.log(serviceName);
+    let results = await pgQuery('SELECT * FROM Logs;');
+    return results;
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+// getData('serviceA').then((data)=>console.log(data));
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+    win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, './preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
-  // demo example of endpoint
-  ipcMain.handle('ping', () => 'pong');
+
+  ipcMain.handle('pgLogs', async (event, serviceName) => {
+    // win.webContents.send('res', 'dbLogs');
+    getData(serviceName)
+    .then((data) => {
+      console.log(data);
+      win.webContents.send('res', data);
+    });
+  });
+  
   //loads webpage from
   win.loadFile('./dist/index.html');
 };
@@ -22,6 +56,7 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+  
 });
 
 //Quit the app when all windows are closed (Windows & Linux)
