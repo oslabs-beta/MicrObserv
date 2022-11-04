77b0afde0a70020b48bd32a3414503ff7290b44a
WerdnaOGN <50420651+WerdnaOGN@users.noreply.github.com>
2022-11-03 19:58:41 -0700

    cleaned up code

--- Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
+++ Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
@@ -47,6 +47,3 @@ export default function TracersDisplay() {
   );
 }
 
-const comms = ["Service A", "Service B", "Service A", "Service B", "Service C", "Service B", "Service A", "Service B", "Service A", "Service B", "Service C", "Service B", "Service A", "Service B", "Service A", "Service B", "Service C", "Service B"];
-const fullTime = [70, 180, 90, 250, 310, 100, 70, 180, 90, 250, 310, 100, 70, 1800, 90, 250, 310, 100];
-const returnTime = [40, 70, 50, 20, 120, 50, 40, 70, 50, 20, 120, 50, 40, 70, 50, 20, 120, 50];
