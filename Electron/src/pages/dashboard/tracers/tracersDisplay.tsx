b43d0c7fcc4f32c4954fa8994bee2f88cc2f15c4
WerdnaOGN <50420651+WerdnaOGN@users.noreply.github.com>
2022-11-03 19:44:32 -0700

    connected backend to tracers display

--- Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
+++ Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import DashboardContainer from '../dashboardContainer';
 import BarChart from './barChart';
 
@@ -12,10 +12,37 @@ export default function TracersDisplay() {
   // //create boolean checking if a tracer obj
   // //console.log('message from the server to Tracers Display: ', msg.data);
   // }
+  const [tracers, updateTracers] = useState<any[]>([]);
+  
+  useEffect(() => {
+    //get request for logs
+    const fetchData = async () => {
+      const data = await fetch('/getTracers');
+      const json = await data.json()
+      const jsonParsed = await JSON.parse(json);
+      updateTracers(jsonParsed);
+    }
+    fetchData()
+    },[]);
+   //console.log(tracers)
+  const output: any =[[],[],[]];
+      for(let i = 0; i < 12; i++ ){
+        console.log('forloop hit')
+        if(i % 2 === 0){
+          output[0].push(`${tracers[i] ? tracers[i].src : "loading"} to ${tracers[i] ? tracers[i].dest : "loading"}`)
+          output[1].push(new Date(tracers[i] ? tracers[i].endtime : "").getTime()- new Date(tracers[i] ? tracers[i].starttime: "").getTime())
+        }
+        else{
+          output[2].push(new Date(tracers[i] ? tracers[i].starttime : '').getTime() - new Date(tracers[i] ? tracers[i-1].starttime: "").getTime());
+        }
+      }
+  console.log("starts", output[1]);
+  console.log("ends", output[2]);
+     
   return (
     <div className='overflow-hidden w-full'>
       <DashboardContainer title='Latency'/>
-      <BarChart communications={comms} timeAtoA={fullTime} timeBtoA={returnTime}/>
+      <BarChart communications={output[0]} timeAtoA={output[1]} timeBtoA={output[2]}/>
     </div>
   );
 }
