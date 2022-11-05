5adf0c48c3e220da796acdca8eb3708de1172b6d
Chance <kupersmith3.6@gmail.com>
2022-11-05 18:26:54 -0500

    event driven logs
    Logs occurring in demo successfully are sent to desktop app and db in an event driven arch

--- Backend/model.js
+++ Backend/model.js
@@ -1,16 +1,106 @@
 const { Pool } = require('pg');
 
-const myURI = 'postgres://nzknncbd:AzIp1howQ8DKmTlflRP18UNTisXgzBsa@otto.db.elephantsql.com/nzknncbd';
+// SETUP
+/* connectToDesktopAppDB
+ * Description: connects package to desktop application db and assigns a query function to global variable dbQuery
+ */
+let pool;
+const connectToDesktopAppDB = async () => {
+  let dbQuery;
+  try{    
+      pool = new Pool({
+          connectionString: process.env.PG_URI
+      });
+      dbQuery = (text, params, callback)=> pool.query(text, params, callback);
+  }
+  catch(err) {console.log('Error connecting URI: ', err) }
+  // Create network tracers table
+  try{
+      await dbQuery(`CREATE TABLE IF NOT EXISTS nTracers(
+                      id SERIAL,
+                      nSrc VARCHAR NOT NULL,
+                      dest VARCHAR NOT NULL,
+                      traceId VARCHAR NOT NULL,
+                      nStartTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      nEndTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      nCompleted BOOLEAN DEFAULT FALSE,
+                      PRIMARY KEY(id));`
+                  );
+  }
+  catch(err) {
+      console.log(`${defaultErrorMsg} Problem creating network tracers table in db, Error: ${err}`);
+  }
+  // Create process tracers table
+  try{
+    await dbQuery(`CREATE TABLE IF NOT EXISTS pTracers(
+                    id SERIAL,
+                    pSrc VARCHAR NOT NULL,
+                    traceId VARCHAR NOT NULL,
+                    pStartTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    pEndTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    pCompleted BOOLEAN DEFAULT FALSE,
+                    PRIMARY KEY(id));`
+                );
+  }
+  catch(err) {
+      console.log(`${defaultErrorMsg} Problem creating process tracers table in db, Error: ${err}`);
+  } 
+  // Create update endTime function
+  try{
+      await dbQuery(`CREATE OR REPLACE FUNCTION update_endtime()
+                      RETURNS TRIGGER AS 
+                      $$
+                          BEGIN
+                              NEW.endTime = now();
+                              RETURN NEW;
+                          END;
+                      $$ 
+                      language 'plpgsql'`
+                  );
+  }
+  catch(err) {
+      console.log(`${defaultErrorMsg} Problem creating db function update_endTime, Error: ${err}`);
+  }
+  // Create on nTracers update trigger for endTime function
+  try{
+      await dbQuery(`CREATE TRIGGER update_endtime_on
+                      BEFORE UPDATE
+                      ON nTracers
+                      FOR EACH ROW
+                      EXECUTE PROCEDURE update_endtime();`
+                  );
+  }
+  // Expected trigger already exists error
+  catch(err) {}
+  // Create on nTracers update trigger for endTime function
+  try{
+    await dbQuery(`CREATE TRIGGER update_endtime_on
+                    BEFORE UPDATE
+                    ON pTracers
+                    FOR EACH ROW
+                    EXECUTE PROCEDURE update_endtime();`
+                );
+  }
+  // Expected trigger already exists error
+  catch(err) {}
+  // Create logs table
+  try {
+      await dbQuery(`CREATE TABLE IF NOT EXISTS Logs(
+                      id SERIAL,
+                      src VARCHAR NOT NULL,
+                      msg VARCHAR NOT NULL,
+                      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      PRIMARY KEY(id));`
+                  );
+  }
+  catch(err) {
+      console.log(`${defaultErrorMsg} Problem creating logs table in db, Error: ${err}`);
+  }
+}
 
-const URI = process.env.PG_URI || myURI;
+connectToDesktopAppDB();
 
-const pool = new Pool({
-  connectionString: URI
-});
 
 module.exports = {
-  query: (text, params, callback) => {
-    console.log('executed query', text);
-    return pool.query(text, params, callback);
-  }
+  query: (text, params, callback) => pool.query(text, params, callback)
 };
--- Backend/routes.js
+++ Backend/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const controller = require('./controllers/controller.js');
+const dbController = require('./controllers/dbController');
+const electronController = require('./controllers/electronController');
 const router = express.Router();
 
 const debug = (req, res, next) => {
@@ -7,8 +8,8 @@ const debug = (req, res, next) => {
   return next;
 }
 
-router.post('/newLog', debug, (req, res) => {
-  res.status(200).send(res.locals.id);
+router.post('/newLog', electronController.newLog, dbController.storeLog, (req, res) => {
+  res.sendStatus(200);
 });
 router.post('/newNTracer', debug, (req, res) => {
   res.status(200).send(res.locals.id);
