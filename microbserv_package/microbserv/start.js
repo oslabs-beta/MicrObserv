6c1f3692eb50028cf4cc83e4f20d88efb51f896e
Chance <kupersmith3.6@gmail.com>
2022-11-05 13:39:31 -0500

    Update start.js

--- microbserv_package/microbserv/start.js
+++ microbserv_package/microbserv/start.js
@@ -1,103 +1,87 @@
 // Private variables
-let serviceName, config, dbQuery;
+let serviceName, config;
 const defaultErrorMsg = 'Error occured in microbserv:';
-const { Pool } = require('pg');
-// SETUP
-/* connectToDesktopAppDB
- * Description: connects package to desktop application db and assigns a query function to global variable dbQuery
- * Parameters:
- * - options = config object containing db Uri
- */
-const connectToDesktopAppDB = async () => {
-    try{    
-        const pool = new Pool({
-            connectionString: config.URI
-        });
-        dbQuery = (text, params, callback)=> pool.query(text, params, callback);
-    }
-    catch(err) {console.log('Error connecting URI: ', err) }
-    // Create tracers table
-    try{
-        await dbQuery(`CREATE TABLE IF NOT EXISTS Tracers(
-                        id SERIAL,
-                        src VARCHAR NOT NULL,
-                        dest VARCHAR,
-                        traceId VARCHAR,
-                        sender BOOLEAN NOT NULL,
-                        startTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        endTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        completed BOOLEAN DEFAULT FALSE,
-                        PRIMARY KEY(id));`
-                    );
-    }
-    catch(err) {
-        console.log(`${defaultErrorMsg} Problem creating tracers table in db, Error: ${err}`);
-    } 
-    // Create update endTime function
-    try{
-        await dbQuery(`CREATE OR REPLACE FUNCTION update_endtime()
-                        RETURNS TRIGGER AS 
-                        $$
-                            BEGIN
-                                NEW.endTime = now();
-                                RETURN NEW;
-                            END;
-                        $$ 
-                        language 'plpgsql'`
-                    );
-    }
-    catch(err) {
-        console.log(`${defaultErrorMsg} Problem creating db function update_endTime, Error: ${err}`);
-    }
-    // Create on update trigger for endTime function
-    try{
-        await dbQuery(`CREATE TRIGGER update_endtime_on
-                        BEFORE UPDATE
-                        ON Tracers
-                        FOR EACH ROW
-                        EXECUTE PROCEDURE update_endtime();`
-                    );
-    }
-    // Expected trigger already exists error
-    catch(err) {}
-    // Create logs table
-    try {
-        await dbQuery(`CREATE TABLE IF NOT EXISTS Logs(
-                        id SERIAL,
-                        src VARCHAR NOT NULL,
-                        msg VARCHAR NOT NULL,
-                        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        PRIMARY KEY(id));`
-                    );
-    }
-    catch(err) {
-        console.log(`${defaultErrorMsg} Problem creating logs table in db, Error: ${err}`);
-    }
-}
+const fetch = require('node-fetch');
+
 // TRACERS
-/* createTracer
- * Description: inserts new tracer into db with attributes src, dest, traceID, and sender
+/* createNTracer
+ * Description: sends new 'network tracer' to MicrObserv backend server with attributes src, dest, traceID
+ * Network tracers track time it takes for a request to get sent to and from a service (network speed).
  * Parameters:
  * - src = requestor service
  * - dest = service endpoint
  * - traceID = unique id used as a common key between services for created tracer objects
- * - sender = specifies if created tracer is the sender
  */
-const storeTracer = (name, dest, traceId, sender) => {
+const createNTracer = async tracer => {
     try{
-        return dbQuery(`INSERT INTO Tracers (src, dest, traceId, sender)
-                        VALUES ($1, $2, $3, $4) RETURNING id;`,
-                        [name, dest, traceId, sender]
-                    )
-                    .then(id => id.rows[0].id);
+      const dbId = await fetch('', {
+        method: 'POST',
+        body: tracer
+      });
+      return dbId;
     }
     catch(err){
-        console.log(`${defaultErrorMsg} Problem storing tracer in db, Error: ${err}`);
+        console.log(`${defaultErrorMsg} Problem sending network tracer to server, Error: ${err}`);
     }
 }
+/* updateNTracer
+ * Description: updates network tracer's 'comlpeted' attribute on MicrObserv backend to true.
+ * Network tracers track time it takes for a request to get sent to and from a service (network speed).
+ * Parameters:
+ * - id = DB primary key for updating tracer
+ */
+const updateNTracer = async id => {
+  try{
+    fetch('', {
+      method: 'POST',
+      body: id
+    })
+  }
+  catch(err){
+    console.log(`${defaultErrorMsg} Problem sending network tracer update to server, Error: ${err}`);
+  }
+}
+/* createPTracer
+ * Description: sends new 'process tracer' to MicrObserv backend server with attributes src, dest, traceID
+ * Process tracers track time it takes for a microservice process to complete.
+ * Parameters:
+ * - src = requestor service
+ * - dest = service endpoint
+ * - traceID = unique id used as a common key between services for created tracer objects
+ */
+const createPTracer = async tracer => {
+  try{
+    const dbId = await fetch('', {
+      method: 'POST',
+      body: tracer
+    });
+    return dbId;
+  }
+  catch(err){
+      console.log(`${defaultErrorMsg} Problem sending process tracer to server, Error: ${err}`);
+  }
+}
+/* updatePTracer
+ * Description: updates process tracer's 'comlpeted' attribute on MicrObserv backend to true.
+ * Process tracers track time it takes for a microservice process to complete.
+ * Parameters:
+ * - id = DB primary key for updating tracer
+ */
+const updatePTracer = async id => {
+try{
+  fetch('', {
+    method: 'POST',
+    body: id
+  })
+}
+catch(err){
+  console.log(`${defaultErrorMsg} Problem sending process tracer update to server, Error: ${err}`);
+}
+}
 
+// TRACER EVENT LISTENERS
 /* httpRequestEventListener
- * Description: when a http request is triggered, a tracer object is created which tracks latency.
+ * Description: when a http request is triggered, a network tracer object is created which tracks network latency.
  */
 const httpRequestEventListener = () => {
     // save http.request in a temp variable
@@ -108,23 +92,19 @@ const httpRequestEventListener = () => {
         // create tracerId for synchronizing tracers created on both sender and receiver
         const tracerId = options.href + Date.now(); //option.href = receiver endpoint
         options.headers['id'] = tracerId;
-        // store new tracer in db
-        const promise = storeTracer(serviceName, options.href, tracerId, true);
+        // send new network tracer to server
+        const tracer = {
+          src: serviceName,
+          dest: options.href,
+          tracerId: tracerId
+        };
+        const promise = createNTracer(tracer);
         // create new response event listener for http.request
         const onResponse = async (error, response, body) => {
-            try{
-                // wait until storeTracer promise resolves
-                const id = await Promise.resolve(promise);
-                // update tracer obj's response time
-                await dbQuery(`UPDATE Tracers
-                                SET completed = TRUE
-                                WHERE id = $1;`,
-                                [id]
-                            );
-            }
-            catch(err) {
-                console.log(`${defaultErrorMsg} Problem updating tracer response time in db, Error: ${err}`);
-            }
+            // wait until createNTracer promise resolves
+            const id = await Promise.resolve(promise);
+            // update tracer obj's response time
+            await updateNTracer({id: id});
             // invoke user passed response event listener
             if(callback) return callback(error, response, body);
         };
@@ -137,22 +117,22 @@ const httpRequestEventListener = () => {
     const ogHttps = https.request;
     https.request = (options, callback)=>{
         let newCallback = callback;
-        const promise = createTracer(name);
+        const tracer = {
+          src: serviceName,
+          dest: options.href,
+          tracerId: tracerId
+        };
+        const promise = createNTracer(tracer);
         newCallback = async (error, response, body) => {
-            id = await Promise.resolve(promise);
-            await dbQuery(`UPDATE Tracers
-                            SET completed = TRUE
-                            WHERE id = $1;`,
-                            [id]
-                        );
+            const id = await Promise.resolve(promise);
+            await updateNTracer({id: id});
             if(callback) return callback(error, response, body);
         };
         return ogHttps(options, newCallback);
     }
 }
-
 /* expressServerEventListener
- * Description: For all express server requests, creates a tracer object which tracks latency
+ * Description: For all express server requests, creates a process tracer object which tracks microservice's latency
 */
 const expressServerEventListener = () => {
     // Can track the http request dest by looking at host and href of request
@@ -161,13 +141,16 @@ const expressServerEventListener = () => {
     http.createServer = (options, requestListener) => {
         let server = ogCreateServer(options, requestListener);
         server.on('request', (request, response)=>{
-            // Create response tracer in db
-            const promise = createTracer(name, name, request.headers.id, false);
+            // Send new process tracer to server
+            const tracer = {
+              src: serviceName,
+              dest: options.href,
+              tracerId: tracerId
+            };
+            const promise = createPTracer(tracer);
             const callback = async (error, response, body) => {
                 id = await Promise.resolve(promise);
-                await dbQuery(`UPDATE Tracers
-                                    SET completed = TRUE
-                                    WHERE id = $1;`, [id]);
+                await updatePTracer({id: id});
             };
             // set event listener for when response is sent to client
             response.on('finish', callback);
@@ -175,23 +158,24 @@ const expressServerEventListener = () => {
         return server;
     }
 }
-// LOGS
-/* storeLog
- * Description: Helper function to insert console log messages into database.
- * Parameters:
- * - src: Service name console log originated from.
- * - msg: actual console logged message.
- */
-const storeLog = (src, msg) => {
-    try{
-        dbQuery(`INSERT INTO Logs (src, msg)
-                 VALUES ($1, $2);`, [src, msg]);
-    }
-    catch(err){
-        console.log(error);
-    }
-}
 
+// LOGS
+/* createLog
+ * Description: Helper function to send console log messages to MicrObserv backend server.
+ * Parameters:
+ * - log = log object to send to server
+ */
+const createLog = async (log) => {
+  try{
+    await fetch('', {
+      method: 'POST',
+      body: log
+    });
+  }
+  catch(err){
+      console.log(`${defaultErrorMsg} Problem sending log to server, Error: ${err}`);
+  }
+}
 /* consoleLogEventListener
  * Description: Event listener that adds any console logs to database as well as logging to terminal
  */
@@ -199,14 +183,19 @@ const consoleLogEventListener = () => {
     const console = require('console');
     const log = console.log;
     console.log = (message, args) =>{
-        storeLog(serviceName, message);
-        if(args) return log(message, args);
-        return log(message);
+        const newLog = {
+          src: serviceName,
+          msg: message
+        }
+        const promise = createLog(newLog);
+        if(args) log(message, args);
+        log(message);
+        Promise.resolve(promise);
     }
 }
 
 /* start
- * Description: Starts up event listeners and connects to desktop application database for synchonization.
+ * Description: Initializes event listeners.
  * Parmeters:
  * - options: user defined configurations
  * - name: Service name
