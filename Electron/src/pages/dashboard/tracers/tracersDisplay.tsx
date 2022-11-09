18439f443236897f602b5df0c096a5494da49672
Chance <kupersmith3.6@gmail.com>
2022-11-09 11:37:09 -0600

    Added graph history component and functionality

--- Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
+++ Electron/src/pages/dashboard/tracers/tracersDisplay.tsx
@@ -2,16 +2,14 @@ import React, { useEffect, useState} from 'react';
 import DashboardContainer from '../dashboardContainer';
 import BarChart from './barChart';
 
-let ws;
-export default function TracersDisplay() {
+// TracersDisplay
+export default props => {
   const [tracerNames, updateTracerNames] = useState<any>([]);
   const [nTracerVals, updateNTracerVals] = useState<any>([]);
   const [pTracerVals, updatePTracerVals] = useState<any>([]);
   useEffect(() => {
-    if(!ws){
-      ws = new WebSocket('ws://localhost:3001/');
-      ws.onopen = () => console.log("connected to websocket server in Logs Display");
-    }
+    let ws = new WebSocket('ws://localhost:3001/');
+    ws.onopen = () => console.log("connected to websocket server in Logs Display");
     //when there is an incoming msg
     ws.onmessage = (msg) => {
       console.log('RECIEVED MSG Tracers!');
@@ -23,15 +21,76 @@ export default function TracersDisplay() {
       if(Array.isArray(tracers.nTracerVals)) updateNTracerVals(nTracerVals => [...nTracerVals, ...tracers.nTracerVals]);
       if(Array.isArray(tracers.pTracerVals)) updatePTracerVals(pTracerVals => [...pTracerVals, ...tracers.pTracerVals]);
     }
-  });
+  }, []);
   return (
     <div className='overflow-hidden w-full'>
       <DashboardContainer title='Latency'/>
       <BarChart communications={tracerNames} timeAtoA={nTracerVals} timeBtoA={pTracerVals}/>
+      <div className='flex justify-center'>
+        <button className='btn' onClick={()=>props.showRealTime(false)}>Next</button>
+      </div>
     </div>
   );
 }
 
-const comms = ["Service A", "Service B", "Service A", "Service B", "Service C", "Service B", "Service A", "Service B", "Service A", "Service B", "Service C", "Service B", "Service A", "Service B", "Service A", "Service B", "Service C", "Service B"];
-const fullTime = [70, 180, 90, 250, 310, 100, 70, 180, 90, 250, 310, 100, 70, 1800, 90, 250, 310, 100];
-const returnTime = [40, 70, 50, 20, 120, 50, 40, 70, 50, 20, 120, 50, 40, 70, 50, 20, 120, 50];
+
+
+// TracersDisplayHistory
+const getTracerHist = (updateTracerNames, updateNTracerVals, updatePTracerVals, next) => {
+  fetch('/MicrObserv/tracerHist', {
+    method: 'POST',
+    body: JSON.stringify({
+      next: next,
+      prev: !next
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.tracers)
+    updateTracerNames(data.tracers.names);
+    updateNTracerVals(data.tracers.nTracerVals);
+    updatePTracerVals(data.tracers.pTracerVals);
+  })
+  .catch(err => console.log(err));
+}
+
+let page;
+const TracersDisplayHistory = props => {
+  const [tracerNames, updateTracerNames] = useState<any>([]);
+  const [nTracerVals, updateNTracerVals] = useState<any>([]);
+  const [pTracerVals, updatePTracerVals] = useState<any>([]);
+  useEffect(() => {
+    page = 1;
+    getTracerHist(updateTracerNames, updateNTracerVals, updatePTracerVals, false);
+  }, []);
+  
+  return (
+    <div className='overflow-hidden w-full'>
+      <DashboardContainer title='Latency'/>
+      <BarChart communications={tracerNames} timeAtoA={nTracerVals} timeBtoA={pTracerVals}/>
+      <div className='flex justify-center'>
+      <button className='btn' onClick={
+            ()=>{
+              page--;
+              page > 0 ? 
+              getTracerHist(updateTracerNames, updateNTracerVals, updatePTracerVals, false)
+              : props.showRealTime(true);
+            }
+          }>Prev
+        </button>
+        <button className='btn' onClick={
+            ()=>{
+              page++;
+              getTracerHist(updateTracerNames, updateNTracerVals, updatePTracerVals, true)
+            }
+          }>Next
+        </button>
+      </div>
+    </div>
+  );
+}
+export { TracersDisplayHistory };
