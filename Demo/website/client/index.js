c88e6cc9a5d666f9f2a5dcbe6a6d0592e50a8a4c
WerdnaOGN <50420651+WerdnaOGN@users.noreply.github.com>
2022-11-10 09:52:15 -0800

    styling completed

--- Demo/website/client/index.js
+++ Demo/website/client/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as ReactDOM from 'react-dom';
 
 import "./styles"
 
 
 const Demo = () => {
+
+    const [time, updateTime] = useState();
+    const [timeOut, updateTimeOut] = useState();
+
+    
     const startDemo =() =>{
-        fetch("http://localhost:8082/start",
+        fetch(`http://localhost:8082/start?reqTime=${time}&timeOut=${timeOut}`,
         {
             mode: 'no-cors',
             headers: {
@@ -15,10 +20,11 @@ const Demo = () => {
         })
         .then(data => {
             console.log(data.json())
-            data.json()
+            return data.json()
         })
         .then(data => {
             console.log("demo started",data)
+            return data;
         })
         
     }
@@ -36,7 +42,12 @@ const Demo = () => {
             <div className="flex justify-center navbar bg-base-100">
                 <a className="btn btn-ghost normal-case text-xl">MicrObserv Demo</a>
             </div>
-            <div className = "flex justify-center gap-3">
+            <div className="flex flex-col items-center gap-2">
+            <input type="number"  placeholder="Request Time Interval" className="input input-bordered w-full max-w-xs" onChange={(e) => updateTime(e.target.value)} />
+            <input type="number" placeholder="Time Out Interval" className="input input-bordered w-full max-w-xs" onChange={(e) => updateTimeOut(e.target.value)} />
+               
+            </div>
+            <div className = "flex justify-center gap-2 p-4">
             <button className="btn btn-outline" onClick={()=>startDemo()}>Start</button>
             <button className="btn btn-outline"onClick={()=>stopDemo()}>Stop</button>
             </div>
